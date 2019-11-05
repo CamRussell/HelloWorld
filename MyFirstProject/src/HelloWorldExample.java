83024b7a8a30e3bb99d92afaf34f9432b0c236ad
CamRussell <cameraha@gmail.com>
2019-11-05 20:12:59 +1300

    Updating with branch text

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// Output This is a test using a new branch 
 		System.out.println("Hello World");
 	}
 
